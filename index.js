6d5bae47bbda5a35e77acc47f4aa26e5be1835e7
jakaria <jakaria9499@gmail.com>
2026-02-03 23:05:14 +0600

    server side config

--- index.js
+++ index.js
@@ -1,14 +1,84 @@
 const express = require('express');
+const cors = require('cors');
+
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 3000;
 
 
 
+app.use(cors());
+app.use(express.json());
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.hattdo5.mongodb.net/?appName=Cluster0`;
+
 app.get('/',(req, res)=>{
-    res.send("pawmart server is running");
+    res.send("pawMart server is running");
 })
 
 
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+
+async function run() {
+    try{
+        await client.connect();
+        await client.db("pawMart").command({ping: 1});
+
+        const db = client.db("pawMart");
+        const petsListsCollection = db.collection('listings');
+        const ordersCollection = db.collection('orders');
+
+
+        app.get('/petsSupplies', async(req, res)=>{
+            const cursor = petsListsCollection.find();
+            const result = await cursor.toArray();
+            res.send(result);
+        })
+       app.get('/petsSupplies/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = {_id: new ObjectId(id)};
+            const result = await petsListsCollection.findOne(query);
+            res.send(result);
+        })
+        app.get('/myOrders', async(req, res) => {
+            const email = req.query.email;
+            const query = {};
+            if(email) {query.email = email}
+            else return
+            const cursor = ordersCollection.find(query);
+            const result = await cursor.toArray();
+            res.send(result);
+        })
+
+        app.post('/addList', async(req, res) => {
+            const newList = req.body;
+            const result = await petsListsCollection.insertOne(newList);
+            res.send(result);
+        })
+        app.post('/orders', async(req, res) => {
+            const newOrder = req.body;
+            const result = await ordersCollection.insertOne(newOrder);
+            res.send(result);
+
+        })
+
+
+    }
+    finally {
+
+    }
+}
+run().catch(console.dir);
+
+
 app.listen(port, ()=>{
-    console.log(`pawmart server is running on port: ${port}`);
+    console.log(`pawMart server is running on port: ${port}`);
 })
