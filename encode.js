798ef4dca17dc9d18d260415bd1bc88daa3d03c1
jakaria <jakaria9499@gmail.com>
2026-02-06 23:10:07 +0600

    vercel deploy completed

--- encode.js
+++ encode.js
@@ -1,5 +1,5 @@
 // encode.js
 const fs = require("fs");
-const key = fs.readFileSync("./pawmart-firebase-admin-key.json", "utf8");
+const key = fs.readFileSync("./pawMart-firebase-admin-key.json", "utf8");
 const base64 = Buffer.from(key).toString("base64");
 console.log(base64);
