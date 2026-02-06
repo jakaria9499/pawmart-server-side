58a040e79d6bd093f8eb87fea91758b271d8c0ac
jakaria <jakaria9499@gmail.com>
2026-02-06 19:39:46 +0600

    added some api and vercel server setup

--- index.js
+++ index.js
@@ -8,7 +8,9 @@ const port = process.env.PORT || 3000;
 
 const admin = require("firebase-admin");
 
-const serviceAccount = require("./pawmart-firebase-admin-key.json");
+// index.js
+const decoded = Buffer.from(process.env.FIREBASE_SERVICE_KEY, "base64").toString("utf8");
+const serviceAccount = JSON.parse(decoded);
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
